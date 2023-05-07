88afad3b3526732ec700ff3f1b8e09284e955487
Livingsaint2 <ofukowoicho2@gmail.com>
2023-05-07 02:18:11 -0700

    101 locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Defines attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
+
