a5ec9059ad3052f2fa93bdf6c67a8c5bc6487c3a
Livingsaint2 <ofukowoicho2@gmail.com>
2023-05-18 23:06:02 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""a script that write to a file"""
+"""Write to a file"""
 
 
 def write_file(filename="", text=""):
-    """Write to a UTF8 text file.
+    """Write a string to a UTF8 text file.
     Args:
-        filename (str): The file name.
+        filename (str): The name of the file to write.
         text (str): The text to write to the file.
     Returns:
-         characters written.
+        The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
