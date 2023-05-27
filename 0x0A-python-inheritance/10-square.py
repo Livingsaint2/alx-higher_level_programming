022eb7effc69056ca63c3a58e65b1359cf922047
Livingsaint2 <ofukowoicho2@gmail.com>
2023-05-27 11:06:31 -0700

    10 square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Starting module to define a rectangle with basegeometry
+attributes and methods
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """Square class to represent a rect with width and height equal """
+
+    def __init__(self, size):
+        """ Instantiate square """
+        self.integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        """ Calcs the rect area """
+        return (self.__size * self.__size)
