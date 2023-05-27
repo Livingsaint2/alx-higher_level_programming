35bb31aef885846c4641c63cdaff22fa790088d0
Livingsaint2 <ofukowoicho2@gmail.com>
2023-05-27 10:55:09 -0700

    6 base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Starting module to define base geometry class
+"""
+
+
+class BaseGeometry():
+    """"
+    Template for a base geometry object
+    """
+    def __init__(self):
+        """ Initial constructor space """
+        pass
+
+    def area(self):
+        """ Space to calculate the area (in progress) """
+        raise Exception("area() is not implemented")
