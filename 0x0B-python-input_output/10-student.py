970c305f7e30b9a4c6559409e755991797a2499d
Livingsaint2 <ofukowoicho2@gmail.com>
2023-05-28 01:58:15 -0700

    10 student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Example of creation of class student
+"""
+
+
+class Student():
+    """
+    This template represents a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Constructor for students"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """Returns dict of students"""
+        if type(attr) is not list:
+            return(self.__dict__)
+        if (not all(isinstance(items, str) for items in attr)):
+            return(self.__dict__)
+        result = {}
+        return({key: self.__dict__[key] for
+                key in self.__dict__.keys() & attr})
