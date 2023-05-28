1092867bb55892ab63b15d00e0e8613982b63bc6
Livingsaint2 <ofukowoicho2@gmail.com>
2023-05-28 01:46:58 -0700

    7 add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """this file receives the standard input"""
 import sys
 import os.path
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == '__main__':
