92c5cbf59291ca545841c1b58b6a1fa10863d752
Livingsaint2 <ofukowoicho2@gmail.com>
2023-06-14 00:56:24 -0700

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 for (i = 0; i < phrases.length; i++) {
   console.log(phrases[i]);
