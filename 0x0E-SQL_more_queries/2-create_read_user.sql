ccaa7d3731cedc8dcd9eb86339073a94d95c3ccb
Livingsaint2 <ofukowoicho2@gmail.com>
2023-06-26 09:55:53 -0700

    Task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- create database hbtn_0d_2 if not exists
+-- create user user_0d_2 with pwd user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
