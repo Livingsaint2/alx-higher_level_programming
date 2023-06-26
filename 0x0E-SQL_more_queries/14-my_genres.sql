bfae78a51d0e6b447d1eecd9b33bb0c6209f1da0
Livingsaint2 <ofukowoicho2@gmail.com>
2023-06-26 12:07:30 -0700

    Task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- lists all genres of the show Dexter
+SELECT genres.name
+FROM tv_show_genres AS show_genres
+INNER JOIN tv_shows AS shows ON show_genres.show_id = shows.id AND shows.title = 'Dexter'
+INNER JOIN tv_genres AS genres ON genres.id = show_genres.genre_id
+ORDER BY genres.name;
