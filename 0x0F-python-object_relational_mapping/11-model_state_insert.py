0a2c14d665ae2977a5363802753907655fceca90
Livingsaint2 <ofukowoicho2@gmail.com>
2023-07-04 07:12:56 -0700

    Task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""inserts a State object into the database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        )
+    )
+    session = sessionmaker(bind=engine)()
+    lou = State(name='Louisiana')
+    session.add(lou)
+    session.commit()
+    print(lou.id)
